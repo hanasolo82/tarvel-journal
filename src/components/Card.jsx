c376b446d69c9a65298adfeca40a0a0a9d4797b6
Hanasolo <hanasolo@hotmail.com>
2024-10-05 14:14:36 +0200

    static card started

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,15 @@ import React from "react";
 export default function Card() {
   return (
     <div>
-        
-        <h2>Card</h2>
+        <img className="card-image" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="" />
+        <img className="point-card" src="./public/point.svg" alt="" />
+        <span className="card-country">JAPAN</span>
+        <a href="www.google.maps" alt="google maps link" className="google">View on Google Maps</a>
+        <h2 className="card-place">Mount Fuji</h2>
+        <h3 className="card-date">12 Jan, 2021 - 24 Jan, 2021</h3>
+        <h3 className="card-text">
+          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+        </h3>
     </div>
     )
 }
