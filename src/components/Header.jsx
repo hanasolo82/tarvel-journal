76f32413f47b25a2641528373cc7a2e2c9462445
Hanasolo <hanasolo@hotmail.com>
2024-10-05 13:46:46 +0200

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+        <img className="header-globe"src="public\globe.svg"/>
+        <h2 className="header-text">my travel journal.</h2>
+    </div>
+    )
+}
