8adc9f1493b02f68b3cedfce674954a926583301
Hanasolo <hanasolo@hotmail.com>
2024-10-06 10:13:52 +0200

    cardd component finished

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,16 +2,29 @@ import React from "react";
 
 export default function Card() {
   return (
-    <div>
-        <img className="card-image" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="" />
-        <img className="point-card" src="./public/point.svg" alt="" />
-        <span className="card-country">JAPAN</span>
-        <a href="www.google.maps" alt="google maps link" className="google">View on Google Maps</a>
+    <div className="card-img-container">
+      <img
+        className="card-image"
+        src="https://scrimba.com/links/travel-journal-japan-image-url"
+        alt=""
+      />
+      <div className="card-text-container">
+          <div className="text-header">
+            <img className="point-card" src="./public/point.svg" alt="" />
+            <span className="card-country">JAPAN</span>
+            <a href="https://www.google.es/maps/place/Monte+Fuji/@35.3606421,138.7170422,2954m/data=!3m2!1e3!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu&g_ep=EgoyMDI0MTAwMi4xIKXMDSoASAFQAw%3D%3D" alt="google maps link" target="_blank" rel="noopener noreferrer" className="google">
+            View on Google Maps
+            </a>
+          </div>
+       
         <h2 className="card-place">Mount Fuji</h2>
         <h3 className="card-date">12 Jan, 2021 - 24 Jan, 2021</h3>
         <h3 className="card-text">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
+          (12,380 feet). Mount Fuji is the single most popular tourist site in
+          Japan, for both Japanese and foreign tourists.
         </h3>
+      </div>
     </div>
-    )
+  );
 }
---
+++ src/components/data.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        id: 1,
+        country: "JAPAN",
+        link:"https://www.google.es/maps/place/Monte+Fuji/@35.3606421,138.7170422,2954m/data=!3m2!1e3!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu&g_ep=EgoyMDI0MTAwMi4xIKXMDSoASAFQAw%3D%3D",
+        place: "Mount Fuji",
+        dateStart: "12 Jan, 2021",
+        dateEnd: "24 Jan, 2021",
+        details: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+
+    }
+]
