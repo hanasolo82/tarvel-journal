2e026c63179d01a5e306bb4e2260e36358568e1a
Hanasolo <hanasolo@hotmail.com>
2024-10-06 17:22:09 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from 'react';
 import Header from './components/Header';
 import Card from './components/Card';
+import data from './data'
 import './App.css';
 
 function App() {
+ const cardList = data.map(item => {
+   return ( 
+    	<Card 
+        key={item.id}
+        {...item}
+        
+      />
+    )
+  })
  
 
   return (
     <div className='container'>
       <Header />
-      <Card />
+      {cardList}
     </div>
   )
 }
