7dcba79729302d3fb1f387bfd8e4b49f481b9c36
Hanasolo <hanasolo@hotmail.com>
2024-10-06 10:40:24 +0200

    datajs

--- src/components/data.js
+++ src/components/data.js
@@ -3,10 +3,33 @@ export default [
         id: 1,
         country: "JAPAN",
         link:"https://www.google.es/maps/place/Monte+Fuji/@35.3606421,138.7170422,2954m/data=!3m2!1e3!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu&g_ep=EgoyMDI0MTAwMi4xIKXMDSoASAFQAw%3D%3D",
+        img:"https://scrimba.com/links/travel-journal-japan-image-url", 
         place: "Mount Fuji",
         dateStart: "12 Jan, 2021",
         dateEnd: "24 Jan, 2021",
         details: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
 
-    }
+    },
+    { 
+        id: 2,
+        country: "AUSTRALIA",
+        link: "https://www.google.es/maps/place/%C3%93pera+de+S%C3%ADdney/@-33.8567799,151.2127164,752m/data=!3m2!1e3!4b1!4m6!3m5!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0?entry=ttu&g_ep=EgoyMDI0MTAwMi4xIKXMDSoASAFQAw%3D%3D",
+        img:"https://scrimba.com/links/travel-journal-australia-image-url", 
+        place: "Sydney Opera House",
+        dateStart: "27 May, 2021",
+        dateEnd: "8 Jun, 2021",
+        details: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+
+    },
+    { 
+        id: 3,
+        country: "NORWAY",
+        link: "https://www.google.es/maps/place/Fiordo+de+Geiranger/@62.1049044,6.9920663,13556m/data=!3m2!1e3!4b1!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi?entry=ttu&g_ep=EgoyMDI0MTAwMi4xIKXMDSoASAFQAw%3D%3D",
+        img:"https://scrimba.com/links/travel-journal-norway-image-url", 
+        place: "Geirangerfjord",
+        dateStart: "01 Oct, 2021",
+        dateEnd: "18 Nov, 2021",
+        details: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+
+    },
 ]
